e85c20d35b308ef6afbccd84dd8ba71d9e5c0167
JeanJAKK <apotrekokoujean06@gmail.com>
2025-12-29 12:08:30 +0000

    CmCP pour FichierSimple final

--- gestion-fichiers/src/gestion/fichiers/cli/CmCP.java
+++ gestion-fichiers/src/gestion/fichiers/cli/CmCP.java
@@ -6,6 +6,9 @@ package gestion.fichiers.cli;
 
 import gestion.fichier.metier.Fichier;
 import gestion.fichier.metier.Repertoire;
+import java.io.FileNotFoundException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Arrays;
 
 
 
@@ -15,34 +18,72 @@ import gestion.fichier.metier.Repertoire;
  */
 public class CmCP extends Commande {
     
-    private String nom;       // nom du fichier à copier
-    private String chemin;
+    private String source;     
+    private String destination;
     
     
-   @Override
+    @Override
     public void executer() {
-        //recuperer le repertoire courant
+        try{
+            String[] tb = source.split("/");
+        String moveElement = tb[tb.length-1];
+        String[] sourceParentTb = Arrays.copyOfRange(tb, 0, tb.length-1);
+        String sourceParent = String.join("/", sourceParentTb);
+
         Repertoire repertoireCourant = Navigateur.getInstance().getRepertoireCourant();
-        
-        for(Fichier f : Navigateur.getInstance().getRepertoireCourant().getFichier()){
-            if(f.getNom().equals(nom)){
-                f.copie(chemin);
+        Fichier.getRoot();
+        try {
+            Navigateur.getInstance().changerRepertoire(sourceParent);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+
+        // Trouver la source
+        Fichier cible = null;
+        for (Fichier f : repertoireCourant.getFichier()) {
+            if (f.getNom().equals(moveElement)) {
+                cible = f;
+                break;
             }
         }
 
-        // REVENIR AU DEPART
-        Navigateur.getInstance().setRepertoireCourant(repertoireCourant);
-    }
-    
+        if (cible == null) {
+            System.err.println("Source introuvable : " + moveElement);
+            return;
+        }
 
+        // Copier
+        try {
+            cible.copie(destination);
+        } catch (FileNotFoundException e) {
+            System.err.println("Destination introuvable : " + e.getMessage());
+        } catch (FileAlreadyExistsException e) {
+            System.err.println("Conflit : " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }finally{
+            Navigateur.getInstance().setRepertoireCourant(repertoireCourant);
+        }
+        
+    }catch(NullPointerException e){
+            System.err.println(e.getMessage());
+    }
+        
+    }
     @Override
     public void setPararmetres(String[] parametres) {
-        this.nom = parametres[0];
-        try{
-            this.chemin = parametres[1];
+        if (parametres.length > 1){
+            try{
+            this.source = parametres[0];
+            this.destination = parametres[1];
         }catch(Exception e){
-            System.err.println("Erreur:" + e.getMessage());
+            System.err.println("cp <source> <destination>" + e.getMessage());
+        } 
+        }else{
+            System.err.println("cp <source> <destination>");
+
         }
+       
         
     } 
 }
